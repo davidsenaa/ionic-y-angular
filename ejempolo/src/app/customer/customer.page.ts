fc17e5043c6a336030fc884c74dfb267c3ff4e9e
davidpuerto <david_apuerto@soy.sena.edu.co>
2026-04-14 16:11:00 -0500

    modo oscuro iconic y angular

--- ejempolo/src/app/customer/customer.page.ts
+++ ejempolo/src/app/customer/customer.page.ts
@@ -1,22 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonSegmentView, IonSegmentContent } from '@ionic/angular/standalone';
+import {
+  InfiniteScrollCustomEvent,
+  IonAvatar, IonContent, IonInfiniteScroll, IonInfiniteScrollContent,
+  IonItem, IonLabel, IonList, IonHeader, IonTitle, IonToolbar,
+  IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent,
+  IonAlert, IonButton
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.page.html',
   styleUrls: ['./customer.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonSegmentButton, IonSegment, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSegment, IonSegmentView, IonSegmentContent]
+  imports: [
+    CommonModule, FormsModule,
+    IonAvatar, IonContent, IonInfiniteScroll, IonInfiniteScrollContent,
+    IonItem, IonLabel, IonList, IonHeader, IonTitle, IonToolbar,
+    IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent,
+    IonAlert, IonButton
+  ]
 })
 export class CustomerPage implements OnInit {
 
-  segmento: string= 'primero';
+  segmento: string = 'primero';
 
-  constructor() { }
+  alertButtons = [
+    { text: 'Cancelar', role: 'cancel' },
+    { text: 'Confirmar', handler: () => console.log('Confirmado') },
+  ];
+
+  items: string[] = [];
 
   ngOnInit() {
+    this.generateItems();
   }
 
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(`Item ${count + i}`);
+    }
+  }
+
+  onIonInfinite(event: InfiniteScrollCustomEvent) {
+    this.generateItems();
+    setTimeout(() => {
+      event.target.complete();
+    }, 500);
+  }
 }
--- ejempolo/src/app/home/home.page.ts
+++ ejempolo/src/app/home/home.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonIcon, IonLabel, IonToggle } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { moon } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonToggle,IonLabel,IonIcon,IonItem,IonContent,IonHeader,IonTitle,IonToolbar,CommonModule,FormsModule],
 })
+
 export class HomePage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ moon });
   }
 
+  toggleDarkMode(event: any) {
+    const darkMode = event.detail.checked;
+
+    if (darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }
+
+  ngOnInit() {}
 }
