8b623a4b033f49a799ac0bdf1035ad765d472e69
davidpuerto <david_apuerto@soy.sena.edu.co>
2026-04-14 12:46:25 -0500

    correcion de colores e integracion de imagenes

--- ejempolo/src/app/app.component.ts
+++ ejempolo/src/app/app.component.ts
@@ -2,7 +2,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
+import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { Icon } from 'ionicons/dist/types/components/icon/icon';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, home, archive } from 'ionicons/icons';
--- ejempolo/src/app/customer/customer.page.ts
+++ ejempolo/src/app/customer/customer.page.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonSegmentView, IonSegmentContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.page.html',
   styleUrls: ['./customer.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonSegmentButton, IonSegment, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSegment, IonSegmentView, IonSegmentContent]
 })
 export class CustomerPage implements OnInit {
+
+  segmento: string= 'primero';
 
   constructor() { }
 
